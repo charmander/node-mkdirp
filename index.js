fe161e1580bdd16e6508c414ab571344d596ddc5
Charmander <~@charmander.me>
2014-12-16 20:54:13 -0800

    Cleanup.

--- index.js
+++ index.js
@@ -1,51 +1,63 @@
+'use strict';
+
 var path = require('path');
 var fs = require('fs');
 
-function mkdirP(p, opts, f, made) {
-    if (typeof opts === 'function') {
-        f = opts;
-        opts = {};
-    }
-    else if (!opts || typeof opts !== 'object') {
-        opts = { mode: opts };
+function mkdirP(p, options, callback, made) {
+    if (typeof options === 'function') {
+        callback = options;
+        options = {};
+    } else if (!options || typeof options !== 'object') {
+        options = { mode: options };
     }
 
-    var mode = opts.mode;
-    var xfs = opts.fs || fs;
+    var mode = options.mode;
+    var fs_ = options.fs || fs;
 
     if (mode === undefined) {
-        mode = 0777 & (~process.umask());
+        mode = 0x1ff & ~process.umask();
     }
-    if (!made) made = null;
 
-    var cb = f || function () {};
+    if (!made) {
+        made = null;
+    }
+
+    if (!callback) {
+        callback = function () {};
+    }
+
     p = path.resolve(p);
 
-    xfs.mkdir(p, mode, function (er) {
-        if (!er) {
+    fs_.mkdir(p, mode, function (error) {
+        if (!error) {
             made = made || p;
-            return cb(null, made);
+            return callback(null, made);
         }
-        switch (er.code) {
-            case 'ENOENT':
-                mkdirP(path.dirname(p), opts, function (er, made) {
-                    if (er) cb(er, made);
-                    else mkdirP(p, opts, cb, made);
-                });
-                break;
 
-            // In the case of any other error, just see if there's a dir
-            // there already.  If so, then hooray!  If not, then something
-            // is borked.
-            default:
-                xfs.stat(p, function (er2, stat) {
-                    // if the stat fails, then that's super weird.
-                    // let the original error be the failure reason.
-                    if (er2 || !stat.isDirectory()) cb(er, made)
-                    else cb(null, made);
-                });
-                break;
+        if (error.code === 'ENOENT') {
+            mkdirP(path.dirname(p), options, function (error, made) {
+                if (error) {
+                    callback(error, made);
+                } else {
+                    mkdirP(p, options, callback, made);
+                }
+            });
+
+            return;
         }
+
+        // In the case of any other error, just see if there's a dir
+        // there already.  If so, then hooray!  If not, then something
+        // is borked.
+        fs_.stat(p, function (er2, stat) {
+            // if the stat fails, then that's super weird.
+            // let the original error be the failure reason.
+            if (er2 || !stat.isDirectory()) {
+                callback(error, made);
+            } else {
+                callback(null, made);
+            }
+        });
     });
 }
 
